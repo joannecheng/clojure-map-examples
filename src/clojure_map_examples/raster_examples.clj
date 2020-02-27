b8c44c8637d9eddcc0555a37772b055e83d02cc1
Joanne Cheng <chengjoanne2@gmail.com>
2020-02-27 10:43:00 +0100

    render 3D

--- src/clojure_map_examples/raster_examples.clj
+++ src/clojure_map_examples/raster_examples.clj
@@ -1,10 +1,15 @@
 (ns clojure-map-examples.raster-examples
   (:require [clojisr.v1.r :as r]
             [clojisr.v1.require :refer [require-r]]
+            [clojisr.v1.rserve :as rserve]
             [clojure.java.io :as io]))
 
-(require-r '[rayshader :refer [raster_to_matrix  reduce_matrix_size plot_map
+(rserve/set-as-default!)
+(r/discard-all-sessions)
+
+(require-r '[rayshader :refer [raster_to_matrix reduce_matrix_size
                                ray_shade sphere_shade lamb_shade ;; shaders
+                               plot_map plot_3d ;; plotting functions
                                add_shadow
                                add_water detect_water]]
            '[raster :refer [extract raster]])
@@ -15,6 +20,7 @@
 ;; Extract raster data
 
 (def load-raster (r/r "function(f) raster(f)"))
+(def clear-rgl (r/r "rgl::rgl.clear"))
 
 (def rmnp_dem
   (load-raster (-> (io/resource "data_files/rmnp_dem.tif") io/file str)))
@@ -22,7 +28,7 @@
 (def rmnp-dem-matrix
   (-> rmnp_dem
       (raster_to_matrix)
-      #_(reduce_matrix_size 0.5)))
+      (reduce_matrix_size 0.5)))
 
 ;; simple example
 (-> rmnp-dem-matrix
@@ -42,6 +48,15 @@
 
 ;; adding shadow
 (-> rmnp-dem-matrix
-    (lamb_shade :zscale 18)
+    sphere_shade
+    (add_shadow (lamb_shade rmnp-dem-matrix :zscale 33))
     (add_shadow (ray_shade rmnp-dem-matrix :zscale 33 :sunaltiude 6 :lambert false) 0.3)
     plot_map)
+
+;; Render 3D
+(-> rmnp-dem-matrix
+    sphere_shade
+    (plot_3d rmnp-dem-matrix :zscale 10))
+
+
+(clear-rgl)
