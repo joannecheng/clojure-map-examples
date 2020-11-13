a2d7e3b101640d553b88fc1c774f894c4a472543
Joanne Cheng <chengjoanne2@gmail.com>
2020-11-13 17:17:38 -0500

    Update installation instructions

--- packages_to_install.R
+++ packages_to_install.R
@@ -1,5 +1,10 @@
 install.packages("Rserve", "http://rforge.net")
-install.packages("tidyverse")
+
+install.packages("tidyverse", dependencies=TRUE)
+# If you run into problems installing tidyverse on ubuntu, check out the following blog post
+# https://blog.zenggyu.com/en/post/2018-01-29/installing-r-r-packages-e-g-tidyverse-and-rstudio-on-ubuntu-linux/
+# "the tidyverse package requires the following non-R packages"
+# libcurl4-openssl-dev, libssl-dev, libxml2-dev.
 
 install.packages("sf")
 install.packages("maps")
